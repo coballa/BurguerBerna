a19549230fce12a4c8c10d43c80a1e499d295b6d
Jose <jarg13@alu.ua.es>
2020-06-15 23:50:01 +0200

    build web

---
+++ precache-manifest.da4a8af7b456c7fb2c8cc70309a8cd34.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5821406febd21136fdf390b58d21be",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "84ed80d150aef3902a5d",
+    "url": "/emoji-search/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "9458a8b81e7a1434e46e",
+    "url": "/emoji-search/static/css/main.db4dbf03.chunk.css"
+  },
+  {
+    "revision": "84ed80d150aef3902a5d",
+    "url": "/emoji-search/static/js/2.1c1ab16a.chunk.js"
+  },
+  {
+    "revision": "fa9f8d86bca7ba94baa2c0fa4755314d",
+    "url": "/emoji-search/static/js/2.1c1ab16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9458a8b81e7a1434e46e",
+    "url": "/emoji-search/static/js/main.4bd867d8.chunk.js"
+  },
+  {
+    "revision": "9ae95c2730ea7d67b25e",
+    "url": "/emoji-search/static/js/runtime-main.d9a9db43.js"
+  },
+  {
+    "revision": "7c5b76955f37a7c2675780ebd6aa6310",
+    "url": "/emoji-search/static/media/logo_berna.7c5b7695.png"
+  }
+]);
